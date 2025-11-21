0b48c58594a0aa1b61cef78f3789717ba654b6d0
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2025-11-21 16:47:55 +0530

    sync

--- interview-mode/src/App.tsx
+++ interview-mode/src/App.tsx
@@ -1,71 +1,10 @@
-import NestedCheckbox from "./components/NestedCheckbox/NestedCheckbox"
 import "./App.css"
-import { useState } from "react"
-import FileExplorerExample from "../../machine-coding/FileExplorer/FileExplorerExample"
+
 
 function App() {
-    const inputConfig = {
-        id: 1,
-        label: "all bikes",
-        checked: false,
-        children: [
-            {
-                id: 2,
-                label: "yamaha",
-                checked: false,
-                children: [
-
-                ]
-
-            },
-            {
-                id: 3,
-                label: "royal enfield",
-                checked: false,
-                children: [
-                    {
-                        id: 4,
-                        label: "classic 350",
-                        checked: false,
-                        children: [
-
-                        ]
-
-                    },
-                    {
-                        id: 5,
-                        label: "guerilla 450",
-                        checked: false,
-                        children: [
-
-                        ]
-
-                    }
-                ]
-
-            }
-        ]
-
-    }
-
-    const [config, setConfig] = useState(inputConfig);
-
     return (
         <>
-            {/* promotion */}
-            <h1>
-                Nested Checkbox by Codewithkai
-            </h1>
 
-
-            {/* core code */}
-            <div>
-                {/* <NestedCheckbox
-                    config={config}
-                    onUpdate={setConfig}
-                /> */}
-                <FileExplorerExample />
-            </div>
         </>
     )
 }
